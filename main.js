a22549745ad6ea96a2660f699f0d8f15c6a9b114
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2021-11-23 14:28:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function Facts(){
+    window.location="facts.html";
+}
+
+function Games(){
+    window.location="games.html";
+}
+
+function Write(){
+    window.location="Write.html";
+}
+
+function Satisfaction(){
+    window.location="satisfaction.html";
+}
